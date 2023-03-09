802f2f57d194f8e34877aeac290bf4446fe93920
FaisalMS <fis445@hotmail.com>
2023-03-10 01:45:52 +0300

    Version 0.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("My First App"),
-      ),
-      body: Center(
-        child: Text("Faisal Al Sofyani"),
-      ),
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
     ),
-  ));
+  );
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+          child: Center(
+            child: Text(
+            "Faisal Al Sofyani",
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.w900,
+              fontStyle: FontStyle.italic,
+              color: Colors.grey,
+            ),
+           ),
+          ),
+      ),
+    );
+  }
 }
